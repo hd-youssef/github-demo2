66e872c4a5939e0ebf5493d28f58685fb2bf6049
youssef <44912677hd-youssef@users.noreply.github.com>
2018-11-27 12:37:02 -0500

    added a method
    arraylist method

--- ArrayesListesWorks.java
+++ ArrayesListesWorks.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 public class ArrayesListesWorks{
+
+
    public static void main(String[]args){
 
 
 
+   }
+
+   public static ArrayList<Integer> combinations(ArrayList<Integer> a, ArrayList<Integer> b){
+
+	   return new ArrayList<>();
 
 
    }
